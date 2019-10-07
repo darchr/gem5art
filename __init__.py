50a458d5f6c3345eb0b6e3758a956de678c228a7
Jason Lowe-Power <jason@lowepower.com>
2019-10-07 11:24:27 -0700

    Make from gem5sa import * work
    Signed-off-by: Jason Lowe-Power <jason@lowepower.com>

--- __init__.py
+++ __init__.py
@@ -1,1 +1,10 @@
 """This is the gem5 structured approach package"""
+
+import importlib.util
+
+from . import artifact
+from . import gem5run
+
+# Only import tasks if celery is installed
+if importlib.util.find_spec('celery') is not None:
+    from . import tasks
