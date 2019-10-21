6e5eccccea9cfa62d26a612769b0ba2d447dab56
Jason Lowe-Power <jason@lowepower.com>
2019-10-21 12:54:40 -0700

    Bump artifact and run versions
    Signed-off-by: Jason Lowe-Power <jason@lowepower.com>

--- artifact/setup.py
+++ artifact/setup.py
@@ -10,7 +10,7 @@ with open(Path(__file__).parent / 'README.md', encoding='utf-8') as f:
 
 setup(
     name = "gem5art-artifact",
-    version = "0.1.3",
+    version = "0.1.4",
     description = "Artifacts for gem5art",
     long_description = long_description,
     long_description_content_type='text/markdown',
