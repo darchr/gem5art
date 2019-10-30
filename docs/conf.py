3d79afe2284af5c33260c31ed097387657e51c41
yazakram <yazakram@ucdavis.edu>
2019-10-30 14:08:26 -0700

    revert conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,9 +12,9 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../artifact'))
-sys.path.insert(0, os.path.abspath('../run'))
-sys.path.insert(0, os.path.abspath('../tasks'))
+sys.path.insert(0, os.path.abspath('../../artifact'))
+sys.path.insert(0, os.path.abspath('../../run'))
+sys.path.insert(0, os.path.abspath('../../tasks'))
 
 # -- Project information -----------------------------------------------------
 
