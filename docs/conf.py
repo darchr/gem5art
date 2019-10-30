08ad157bb2c34f7cb8b6040af1a69124ac093a10
Ayaz Akram <yazakram@ucdavis.edu>
2019-10-30 00:16:02 -0700

    Update docs/conf.py (#15)

--- docs/conf.py
+++ docs/conf.py
@@ -55,3 +55,6 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# We need to set this to override the default primary doc: contents.rst
+master_doc = 'index'
