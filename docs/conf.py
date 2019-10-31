ac56cf3e1835038f26798f329e50c4389b05bc3b
yazakram <yazakram@ucdavis.edu>
2019-10-30 21:48:42 -0700

    Update conf.py to render md tables

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,7 @@ release = '0.2.1'
 # ones.
 extensions = ['recommonmark',
               'sphinx.ext.autodoc',
+              'sphinx_markdown_tables',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
