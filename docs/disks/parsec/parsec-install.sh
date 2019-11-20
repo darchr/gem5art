24a612b4edb100d3ac6ea7e25dec406df4ba06ef
Mahyar Samani <msamani@ucdavis.edu>
2019-11-20 12:06:12 -0800

    Fixed the interactive package installation issue. The scripts now create a disk image that can run PARSEC benchmark.

--- docs/disks/parsec/parsec-install.sh
+++ docs/disks/parsec/parsec-install.sh
@@ -1,11 +1,32 @@
-# install build-essential (gcc and g++ included) and gfortran
+cd /home/gem5/
+echo "12345" | sudo apt update
 
-#Compile NPB 
-echo "12345" | sudo apt-get update install build-essential git m4 scons zlib1g zlib1g-dev libprotobuf-dev protobuf-compiler libprotoc-dev libgoogle-perftools-dev python-dev pythontexinfolibx11-devlibxext-devxorg-dev 
+# Allowing services to restart while updating some 
+# libraries.
+sudo apt install -y debconf-utils
+sudo debconf-get-selections | grep restart-without-asking > libs.txt
+sed -i 's/false/true/g' libs.txt
+while read line; do echo $line | sudo debconf-set-selections; done < libs.txt
+sudo rm libs.txt
+##
 
-git clone git@github.com:darchr/parsec-benchmark
+# Installing packages needed to build PARSEC
+sudo apt install -y build-essential 
+sudo apt install -y m4 
+sudo apt install -y git  
+sudo apt install -y python
+sudo apt install -y python-dev
+sudo apt install -y gettext
+sudo apt install -y libx11-dev 
+sudo apt install -y libxext-dev 
+sudo apt install -y xorg-dev 
+sudo apt install -y unzip
+sudo apt install -y texinfo
+## 
 
+# Building PARSEC
+unzip parsec-benchmark.zip 
 cd parsec-benchmark
-./get-inputs
 source env.sh 
 parsecmgmt -a build -p all
+##
