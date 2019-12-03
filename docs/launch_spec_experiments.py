af7b67e66c2177798c0246c0ce3fcd3a9224a0da
Hoa Nguyen <hoanguyen@ucdavis.edu>
2019-12-02 18:41:14 -0800

    Fix minor bugs in spec2006's launch script
    This commit fixes the indentation error caused the launch script
    to only run one benchmark per cpu x size. This commit also
    updates the relevant parts in the tutorial.

--- docs/launch_spec_experiments.py
+++ docs/launch_spec_experiments.py
@@ -88,7 +88,7 @@ linux_repo = Artifact.registerArtifact(
 linux_binary = Artifact.registerArtifact(
     name = 'vmlinux-4.19.83',
     typ = 'kernel',
-    path = 'linux/vmlinux-4.19.83',
+    path = 'linux-4.19.83/vmlinux-4.19.83',
     cwd = './',
     command = '''
         cp linux-configs/config.4.19.83 linux-4.19.83/.config
@@ -134,7 +134,7 @@ if __name__ == "__main__":
     # unavailable benchmarks: 400.perlbench,447.dealII,450.soplex,483.xalancbmk
 
     for cpu in cpus:
-        for size in sizes[cpu]:
+        for size in benchmark_sizes[cpu]:
             for benchmark in benchmarks:
                 run = gem5Run.createFSRun(
                     'gem5/build/X86/gem5.opt', # gem5_binary
@@ -147,8 +147,8 @@ if __name__ == "__main__":
                     'disk-image/spec2006/spec2006-image/spec2006', # disk_image
                     linux_binary, # linux_binary_artifact
                     disk_image, # disk_image_artifact
-                    cpu, benchmark, "test" , # params
+                    cpu, benchmark, size, # params
                     timeout = 5*24*60*60 # 5 days
                 )
-            run_gem5_instance.apply_async((run,))
+                run_gem5_instance.apply_async((run,))
 
