496ccbe207833eb53438b03f9ffe8b779267f4b4
Hoa Nguyen <hoanguyen@ucdavis.edu>
2019-12-26 09:26:19 -0800

    Update spec2017 disk image scripts from spec2006
    Signed-off-by: Hoa Nguyen <hoanguyen@ucdavis.edu>

--- docs/disks/spec2017/install-spec2017.sh
+++ docs/disks/spec2017/install-spec2017.sh
@@ -1,24 +1,24 @@
 # install build-essential (gcc and g++ included) and gfortran
-echo "12345" | sudo apt-get install build-essential gfortran
+echo "12345" | sudo DEBIAN_FRONTEND=noninteractive apt-get install -y build-essential gfortran
 
-# mount the SPEC2006 ISO file and install SPEC to the disk image
+# mount the SPEC2017 ISO file and install SPEC to the disk image
 mkdir /home/gem5/mnt
-mount -o loop -t iso9660 /home/gem5/CPU2006v1.0.1.iso /home/gem5/mnt
-mkdir /home/gem5/spec2006
-echo "y" | /home/gem5/mnt/install.sh -d /home/gem5/spec2006 -u linux-suse101-AMD64
-cd /home/gem5/spec2006
+mount -o loop -t iso9660 /home/gem5/cpu2017-1.1.0.iso /home/gem5/mnt
+mkdir /home/gem5/spec2017
+echo "y" | /home/gem5/mnt/install.sh -d /home/gem5/spec2017 -u linux-x86_64
+cd /home/gem5/spec2017
 . /home/gem5/mnt/shrc
 umount /home/gem5/mnt
-rm -f /home/gem5/CPU2006v1.0.1.iso
+rm -f /home/gem5/cpu2017-1.1.0.iso
 
-# use the gcc42 config as the template 
-cp /home/gem5/spec2006/config/linux64-amd64-gcc42.cfg /home/gem5/spec2006/config/myconfig.cfg
+# use the example config as the template 
+cp /home/gem5/spec2017/config/Example-gcc-linux-x86.cfg /home/gem5/spec2017/config/myconfig.x86.cfg
 
-# Those 'sed' commands replace paths to gcc, g++ and gfortran binary from /usr/local/sles9/gcc42-0325/bin/* to /usr/bin/*
-sed -i "s/\/usr\/local\/sles9\/gcc42-0325\/bin\/gcc/\/usr\/bin\/gcc/g" /home/gem5/spec2006/config/myconfig.cfg
-sed -i "s/\/usr\/local\/sles9\/gcc42-0325\/bin\/g++/\/usr\/bin\/g++/g" /home/gem5/spec2006/config/myconfig.cfg
-sed -i "s/\/usr\/local\/sles9\/gcc42-0325\/bin\/gfortran/\/usr\/bin\/gfortran/g" /home/gem5/spec2006/config/myconfig.cfg
+# Use sed command to replace the default gcc_dir
+sed -i "s/\/opt\/rh\/devtoolset-7\/root\/usr/\/usr/g" /home/gem5/spec2017/config/myconfig.x86.cfg
+
+# Use sed command to remove the march=native flag when compiling
+sed -i "s/-march=native//g" /home/gem5/spec2017/config/myconfig.x86.cfg
 
 # build all SPEC workloads
-runspec --config=myconfig.cfg --action=build all
-
+runcpu --config=myconfig.x86.cfg --define build_ncpus=$(nproc) --action=build all
--- docs/disks/spec2017/runscript.sh
+++ docs/disks/spec2017/runscript.sh
@@ -4,13 +4,14 @@
 # which contains the workload and the size to run. Then it resets the stats before running the workload.
 # Finally, it exits the simulation after running the workload, then it copies out the result file to be checked.
 
-cd /home/gem5/spec2006
+
+cd /home/gem5/spec2017
 source shrc
 # Read workload file
 m5 readfile > workloadfile
 echo "Done reading workloads"
 
-# The workload file should always exists
+# The workload file should always exists    
 echo "Workload detected"
 echo "Reset stats"
 # Exit gem5 to reset the stats and switch to the detailed CPU
@@ -19,12 +20,12 @@ m5 exit
 # Read the name of the workload, the size of the workload and the output folder
 read -r workload size m5filespath < workloadfile
 # Run the workload of the desired size once
-runspec --size $size --iterations 1 --config myconfig.cfg --noreportable --nobuild $workload
+runcpu --size $size --iterations 1 --config myconfig.x86.cfg --noreportable --nobuild $workload
 # Exit gem5 to switch back to KVM
 m5 exit
-    
+
 # Copy the SPEC result files to host
-for filepath in /home/gem5/spec2006/result/*; do
+for filepath in /home/gem5/spec2017/result/*; do
     filename=$(basename $filepath)
     m5 writefile $filepath $m5filespath/$filename
 done
