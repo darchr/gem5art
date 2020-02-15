07584fd60d7f3de176a5f4f2c9bdd5bf4e51b2bf
Ayaz <yazakram@ucdavis.edu>
2020-02-15 00:01:50 -0800

    Update the version to 1.0.0

--- tasks/setup.py
+++ tasks/setup.py
@@ -39,7 +39,7 @@ with open(Path(__file__).parent / 'README.md', encoding='utf-8') as f:
 
 setup(
     name = "gem5art-tasks",
-    version = "0.4.1",
+    version = "1.0.0",
     description = "A celery app for gem5art",
     long_description = long_description,
     long_description_content_type='text/markdown',
