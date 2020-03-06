4f686f40e48a29b5ad7ce4a0e0b1b3382c37a863
Hoa Nguyen <hoanguyen@ucdavis.edu>
2020-03-06 15:00:04 -0800

    Improve spec tutorials (#87)
    * Improve disk image related scripts.
    * Update links in the spec2017 tutorial.

--- docs/disks/spec2006/install-spec2006.sh
+++ docs/disks/spec2006/install-spec2006.sh
@@ -14,11 +14,21 @@ rm -f /home/gem5/CPU2006v1.0.1.iso
 # use the gcc42 config as the template 
 cp /home/gem5/spec2006/config/linux64-amd64-gcc42.cfg /home/gem5/spec2006/config/myconfig.cfg
 
-# Those 'sed' commands replace paths to gcc, g++ and gfortran binary from /usr/local/sles9/gcc42-0325/bin/* to /usr/bin/*
-sed -i "s/\/usr\/local\/sles9\/gcc42-0325\/bin\/gcc/\/usr\/bin\/gcc/g" /home/gem5/spec2006/config/myconfig.cfg
-sed -i "s/\/usr\/local\/sles9\/gcc42-0325\/bin\/g++/\/usr\/bin\/g++/g" /home/gem5/spec2006/config/myconfig.cfg
-sed -i "s/\/usr\/local\/sles9\/gcc42-0325\/bin\/gfortran/\/usr\/bin\/gfortran/g" /home/gem5/spec2006/config/myconfig.cfg
+# these 'sed' commands replace paths to gcc, g++ and gfortran binary from /usr/local/sles9/gcc42-0325/bin/* to /usr/bin/*
+# more about /c: https://www.gnu.org/software/sed/manual/sed.html#index-Replacing-selected-lines-with-other-text
+# more about MacOS compatibility: https://stackoverflow.com/a/18627173
+# the commands replace the whole lines starting with one of {'CC', 'CXX', 'FC'}
+sed -i "/^CC[\ \t]*=/c\\\nCC = \/usr\/bin\/gcc\n" /home/gem5/spec2006/config/myconfig.cfg
+sed -i "/^CXX[\ \t]*=/c\\\nCXX = \/usr\/bin\/g++\n" /home/gem5/spec2006/config/myconfig.cfg
+sed -i "/^FC[\ \t]*=/c\\\nFC = \/usr\/bin\/gfortran\n" /home/gem5/spec2006/config/myconfig.cfg
+
+# reduce the number of output types
+# by default, spec generates output in various file types
+# this command forces the outputs to be a txt file and stdout output
+# this command replaces the whole 'output_format' line
+# more about /c: https://www.gnu.org/software/sed/manual/sed.html#index-Replacing-selected-lines-with-other-text
+# more about MacOS compatibility: https://stackoverflow.com/a/18627173
+sed -i "/^output_format[\ \t]*=/c\\\noutput_format = txt, Screen\n" /home/gem5/spec2006/config/myconfig.cfg
 
 # build all SPEC workloads
 runspec --config=myconfig.cfg --action=build all
-
--- docs/disks/spec2017/install-spec2017.sh
+++ docs/disks/spec2017/install-spec2017.sh
@@ -15,7 +15,10 @@ rm -f /home/gem5/cpu2017-1.1.0.iso
 cp /home/gem5/spec2017/config/Example-gcc-linux-x86.cfg /home/gem5/spec2017/config/myconfig.x86.cfg
 
 # use sed command to replace the default gcc_dir
-sed -i "s/\/opt\/rh\/devtoolset-7\/root\/usr/\/usr/g" /home/gem5/spec2017/config/myconfig.x86.cfg
+# this command replaces the whole 'define gcc_dir' line
+# more about /c: https://www.gnu.org/software/sed/manual/sed.html#index-Replacing-selected-lines-with-other-text
+# more about MacOS compatibility: https://stackoverflow.com/a/18627173
+sed -i "/%[\ \t]*define[\ \t]*gcc_dir/c\\\n%   define  gcc_dir        \"/usr\"\n" /home/gem5/spec2017/config/myconfig.x86.cfg
 
 # use sed command to remove the march=native flag when compiling
 # this is necessary as the packer script runs in kvm mode, so the details of the CPU will be that of the host CPU
@@ -27,7 +30,17 @@ sed -i "s/-march=native//g" /home/gem5/spec2017/config/myconfig.x86.cfg
 # https://www.spec.org/cpu2017/Docs/config.html#sysinfo-program
 # this is necessary as the sysinfo program queries the details of the system's CPU
 # the query causes gem5 runtime error
+# this command adds 'sysinfo_program' with an empty value
+# 'sysinfo_program' should be defined in 'Global Settings'
 sed -i "s/command_add_redirect = 1/sysinfo_program =\ncommand_add_redirect = 1/g" /home/gem5/spec2017/config/myconfig.x86.cfg
+
+# reduce the number of output types
+# by default, spec generates output in various file types
+# this command forces the output to be only a txt file
+# this command replaces the whole 'output_format' line
+# more about /c: https://www.gnu.org/software/sed/manual/sed.html#index-Replacing-selected-lines-with-other-text
+# more about MacOS compatibility: https://stackoverflow.com/a/18627173
+sed -i "/^output_format/c\\\noutput_format        = txt\n" /home/gem5/spec2017/config/myconfig.x86.cfg
 
 # build all SPEC workloads
 runcpu --config=myconfig.x86.cfg --define build_ncpus=$(nproc) --action=build all
