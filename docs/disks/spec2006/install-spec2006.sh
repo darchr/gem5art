68a7c70b6217916a21536871d034bc7f9c3a852d
Hoa Nguyen <hoanguyen@ucdavis.edu>
2020-03-05 18:20:39 -0800

    Update install-spec2006.sh

--- docs/disks/spec2006/install-spec2006.sh
+++ docs/disks/spec2006/install-spec2006.sh
@@ -14,10 +14,13 @@ rm -f /home/gem5/CPU2006v1.0.1.iso
 # use the gcc42 config as the template 
 cp /home/gem5/spec2006/config/linux64-amd64-gcc42.cfg /home/gem5/spec2006/config/myconfig.cfg
 
-# Those 'sed' commands replace paths to gcc, g++ and gfortran binary from /usr/local/sles9/gcc42-0325/bin/* to /usr/bin/*
-sed -i "s/\/usr\/local\/sles9\/gcc42-0325\/bin\/gcc/\/usr\/bin\/gcc/g" /home/gem5/spec2006/config/myconfig.cfg
-sed -i "s/\/usr\/local\/sles9\/gcc42-0325\/bin\/g++/\/usr\/bin\/g++/g" /home/gem5/spec2006/config/myconfig.cfg
-sed -i "s/\/usr\/local\/sles9\/gcc42-0325\/bin\/gfortran/\/usr\/bin\/gfortran/g" /home/gem5/spec2006/config/myconfig.cfg
+# these 'sed' commands replace paths to gcc, g++ and gfortran binary from /usr/local/sles9/gcc42-0325/bin/* to /usr/bin/*
+# more about /c: https://www.gnu.org/software/sed/manual/sed.html#index-Replacing-selected-lines-with-other-text
+# more about MacOS compatibility: https://stackoverflow.com/a/18627173
+# the commands replace the whole lines starting with one of {'CC', 'CXX', 'FC'}
+sed -i "/^CC[\ \t]*=/c\\\nCC = \/usr\/bin\/gcc\n" /home/gem5/spec2006/config/myconfig.cfg
+sed -i "/^CXX[\ \t]*=/c\\\nCXX = \/usr\/bin\/g++\n" /home/gem5/spec2006/config/myconfig.cfg
+sed -i "/^FC[\ \t]*=/c\\\nFC = \/usr\/bin\/gfortran\n" /home/gem5/spec2006/config/myconfig.cfg
 
 # build all SPEC workloads
 runspec --config=myconfig.cfg --action=build all
